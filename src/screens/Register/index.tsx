4966e2cd7a738202c6b78b541c89c1cdba320386
Bruno Azevedo <bruno.ds.azevedo@gmail.com>
2022-03-25 16:14:50 -0300

    criado component de HistoryCards para lidar com listagem de cards na tela de Resumo, e criado a rota para acessar a tela de resumo

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -98,7 +98,6 @@ export function Register() {
 
     try {
       const dataKey = '@gofinance:transactions';
-
       const data = await AsyncStorage.getItem(dataKey);
       const currentData = data ? JSON.parse(data) : [];
 
