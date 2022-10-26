05a0c8d1570246cf9738300443892201bb9c8b2e
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2022-10-26 10:42:56 -0300

    feito atualização de método para logar  com Google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 
-import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+// import * as AuthSession from 'expo-auth-session';
 
 import * as AppleAuthentication from 'expo-apple-authentication';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -40,20 +41,25 @@ function AuthProvider({ children }: AuthProviderProps) {
   const [ user, setUser ] = useState<User>({} as User);
   const [ userStorageLoading, setUserStorageLoading ] = useState(true);
 
+  const [_, response, promptAsync] = Google.useAuthRequest({
+    clientId: CLIENT_ID,
+    redirectUri: REDIRECT_URI
+  });
+
   const userStorageKey = '@gofinances:user';
 
   async function signInWithGoogle() {
     try {
-      const RESPONSE_TYPE = 'token';
-      const SCOPE = encodeURI('profile email');
+      await promptAsync();
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
 
-      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+  async function getGoogleUser(googleCredential: string) {
+    try {
+      const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleCredential}`);
 
-      const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
-      
-      if(type === 'success') {
-        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
-        
         const userInfo = await response.json();
         console.log(userInfo);
         
@@ -71,10 +77,8 @@ function AuthProvider({ children }: AuthProviderProps) {
           photo: userInfo.picture,
         });
         await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
-      }
-
-    } catch (error: any) {
-      throw new Error(error);
+    } catch (error) {
+      throw error;
     }
   }
 
@@ -124,6 +128,13 @@ function AuthProvider({ children }: AuthProviderProps) {
 
     loadUserStorageData();
   }, []);
+
+  useEffect(() => {    
+    if(response && response.type === 'success') {
+      const { access_token } = response.params;
+      getGoogleUser(access_token);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider value={{
